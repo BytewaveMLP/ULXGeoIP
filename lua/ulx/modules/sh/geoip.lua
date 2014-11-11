ce2cfca557a8e2810a1fda41b4dd9fadafc0093d
Zerf <zerfgog202@gmail.com>
2014-11-10 20:09:12 -0700

    Various Improvements/Cleanup (Again)

--- lua/ulx/modules/sh/geoip.lua
+++ lua/ulx/modules/sh/geoip.lua
@@ -1,99 +1,61 @@
-local CATEGORY_NAME = "Utility"
-
 if SERVER then
-	util.AddNetworkString( "ulx_geoip_data" )
+	util.AddNetworkString("geoip_data")
 end
 
 if CLIENT then
-	net.Receive( "ulx_geoip_data",
-		function( len )
-			local rawdata = net.ReadString()
-			local data = util.JSONToTable( rawdata )
+	net.Receive("geoip_data", function(len)
+		local rawdata = net.ReadString()
+		local data = util.JSONToTable(rawdata)
 
-			PrintTable( data )
-		end )
+		PrintTable(data)
+	end)
 end
 
-function ulx.geoip( calling_ply, target_ply )
-	if IsValid(target_ply) then
-		local user_name = target_ply:Nick()
+function geoip_capture(ply, callback, errcallback)
+	local ip = ply:IPAddress():match("%d+%.%d+%.%d+%.%d+") or ""
+	local query_string = "http://www.telize.com/geoip/"..ip
 
-		-- Thanks to Garry, LocalPlayer():IPAddress() returns <IP>:<PORT>. All we want is <IP>.
-		local user_ip = target_ply:IPAddress():match("%d+%.%d+%.%d+%.%d+") or ""
-
-		-- Thanks, http://www.telize.com/ for the wonderful REST API!
-		-- (Switched from FreeGeoIP.net because of better information, considering switching back)
-		local query_string = "http://www.telize.com/geoip/" .. user_ip
-
-		-- Get the JSON table containing the users' location, etc
-		http.Fetch( query_string,
-			function ( json, number, headers, status )
-				local rawdata = json
-				local data = util.JSONToTable( rawdata )
-
-				-- Type consistency checks
-				if not data or type( data ) ~= "table" then
-					if IsValid( calling_ply ) then
-						calling_ply:ChatPrint( "Failed to fetch location - no or invalid data returned." )
-					else
-						ServerLog( "Failed to fetch location - no or invalid data returned." )
-					end
-					return
-				end
-
-				-- Notifications
-				if IsValid( calling_ply ) then
-					calling_ply:ChatPrint( string.format( "Location data for %s (%s) printed to console.", user_name, user_ip ) )
-					-- Note that the information displayed is what they were looking for
-					if SERVER then
-						calling_ply:PrintMessage( HUD_PRINTCONSOLE, string.format( "Location data for %s (%s):", user_name, user_ip ) )
-
-						-- ServerLog( "Sent message ulx_geoip_msg to " .. calling_ply:Nick() )
-					end
-				else
-					-- Tell console that this is in fact the information it's looking for
-					ServerLog( string.format( "Location data for %s (%s):", user_name, user_ip ) )
-				end
-
-				-- Tell target_ply they've been tracked
-				target_ply:ChatPrint( "An admin captured your GeoIP data." )
-
-				if IsValid( calling_ply ) then
-					if SERVER then
-						net.Start( "ulx_geoip_data" )
-							net.WriteString( rawdata )
-						net.Send( calling_ply )
-
-						-- ServerLog( "Sent message ulx_geoip_data to " .. calling_ply:Nick() )
-					end
-				else
-					for k, v in pairs( data ) do
-						ServerLog( string.format( "%s: %s", k, v ) )
-					end
-				end
-			end,
-			function ( status, user_name )
-				-- Display HTTP status code to calling_ply
-				if IsValid( calling_ply ) then
-					calling_ply:ChatPrint( string.format( "Failed to fetch location - HTTP request failed with status code %s.", status ) )
-				else 
-					ServerLog( string.format( "Failed to fetch location - HTTP request failed with status code %s.", status ) )
-				end
-				return
-			end )
-	else
-		-- Target ply validity checks
-		if IsValid( calling_ply ) then
-			calling_ply:ChatPrint( string.format( "Failed to fetch location - %s is not a valid player (possibly disconnected?).", user_name ) )
-		else
-			ServerLog( string.format( "Failed to fetch location - %s is not a valid player (possibly disconnected?).", user_name ) )
+	http.Fetch(query_string,
+		function(json, len, headers, status)
+			callback(json)
+		end,
+		function(err)
+			errcallback(err)
 		end
+	)
+end
+
+function ulx.geoip(calling_ply, target_ply)
+	local user_name = target_ply:Nick()
+	ULib.tsay(calling_ply, "ULX GeoIP: Attempting to fetch GeoIP data for: "..user_name)
+
+	if not IsValid(target_ply) then
+		ULib.tsayError(calling_ply, "ULX GeoIP: Failed to fetch data: "..user_name.." is not a valid player (disconnected?).")
+		return
 	end
 
-	ulx.fancyLogAdmin( calling_ply, "#A captured the GeoIP location data for #T", target_ply )
+	if SERVER then
+		geoip_capture(target_ply,
+		function(data)
+			if not calling_ply:IsPlayer() then for k, v in pairs(util.JSONToTable(data)) do ServerLog(k..":    "..v) end return end
+
+			net.Start("geoip_data")
+				net.WriteString(data)
+			net.Send(calling_ply)
+			ULib.tsay(calling_ply, "ULX GeoIP: Data for "..user_name.." has been printed to console.")
+
+			ulx.fancyLogAdmin(calling_ply, "#A captured the GeoIP location data for #T", target_ply)
+		end,
+		function(err)
+			local errstr = "ULX GeoIP: Failed to fetch data: "..err
+			if not calling_ply:IsPlayer() then ServerLog(errstr) return end
+
+			ULib.tsayError(calling_ply, errstr)
+		end)
+	end
 end
 
-local geoip = ulx.command( CATEGORY_NAME, "ulx geoip", ulx.geoip, "!geoip" )
-geoip:addParam{ type=ULib.cmds.PlayerArg }
-geoip:defaultAccess( ULib.ACCESS_SUPERADMIN )
-geoip:help( "Prints geographical information about a user from their IP to console." )
+local geoip = ulx.command("Utility", "ulx geoip", ulx.geoip, "!geoip")
+geoip:addParam{type = ULib.cmds.PlayerArg}
+geoip:defaultAccess(ULib.ACCESS_SUPERADMIN)
+geoip:help("Prints geographical information about a user from their IP to console.")
